d575a12493e6e73e93f7bd1b3e5b929d8108a97c
Krzysztof <kszyneg@gmail.com>
2022-07-13 19:14:37 +0200

    more excercises

---
+++ src/test/java/ImageTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageTest {
+
+
+    @Test
+    public void imageTest() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+        WebElement image = driver.findElement(By.tagName("img"));
+    //    System.out.println(image.getAttribute("naturalHeight"));
+        String height = image.getAttribute("naturalHeight");
+        // asercja porównuje czy wysokość wynosi zero
+        Assert.assertEquals("0",height);
+
+    }
+}
